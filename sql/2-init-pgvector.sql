5b0c381c28df2725bf3952732a6c6248da347d8a
Alptug Dingil <alptug.dingil@inspired.consulting>
2023-10-17 14:18:14 +0200

    added db init scripts

---
+++ sql/2-init-pgvector.sql
@@ -0,0 +1,8 @@
+-- Connect to the newly created database
+\c aisheAI;
+
+-- Enable the vector extension
+CREATE EXTENSION vector;
+
+-- Create the items table with a vector column
+CREATE TABLE items (id bigserial PRIMARY KEY, embedding vector(3));
