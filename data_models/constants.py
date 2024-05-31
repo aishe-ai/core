c9a453485a3fb089c536205b5651626ba6922494
Alptug Dingil <alptug.dingil@inspired.consulting>
2024-05-31 11:16:56 +0200

    updated example prompts

--- data_models/constants.py
+++ data_models/constants.py
@@ -13,9 +13,9 @@ LOADING_BLOCK = [
 CUSTOM_SLACK_COMMANDS = ["/intragpt-health-check"]
 
 EXAMPLE_PROMPTS = [
-    "- Normal prompts like in chatgpt:\n\t\t\t- Who is Sam Hyde?",
-    "- Google:\n\t\t\t- Wie hoch ist die Regenwahrscheinlichkeit heute in Hannover?\n\t\t\t- Who is Leo DiCaprio's girlfriend? What is her current age raised to the 0.43 power?",
-    "- Website:\n\t\t\t- Wo kann ich auf dieser Website meine Fragen einreichen? https://verwaltung.bund.de/portal/DE/ueber\n\t\t\t- Wann endet die Frist für die erste Phase? https://verwaltung.bund.de/leistungsverzeichnis/de/leistung/99148138017000",
+    "- Normal prompts like in chatgpt:\n\t\t\t- Who is Thomas Gottschalk?",
+    "- Google:\n\t\t\t- How is the weather today in cologne?\n\t\t\t- Who is Leo DiCaprio's girlfriend? What is her current age raised to the 0.43 power?",
+    "- Website:\n\t\t\t- Where can i upload my application on this page? https://verwaltung.bund.de/portal/DE/ueber\n\t\t\t- Wann endet die Frist für die erste Phase? https://verwaltung.bund.de/leistungsverzeichnis/de/leistung/99148138017000",
     "- Documents (with attached file):\n\t\t\t- Summarize this document\n\t\t\t- Translate this file into german\n\n"
     "- Confluence:\n\t\t\t- Summarize the page: https://...... \n\n"
     "- Git repos:\n\t\t\t- In which language is this project written: https://github.com/martinmimigames/little-music-player?\n\t\t\t- What does HWListener in this project https://github.com/martinmimigames/little-music-player do?\n\t\t\t- What is the difference of the m3u branch in this project: https://github.com/martinmimigames/little-music-player?\n\n",
