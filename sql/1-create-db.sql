b7232d9c67fb017abb4bdee9036de719c5500343
Alptug Dingil <alptug.dingil@inspired.consulting>
2024-05-31 12:14:04 +0200

    updated init sql

--- sql/1-create-db.sql
+++ sql/1-create-db.sql
@@ -1,2 +1,5 @@
+-- Create the 'postgres' role with all permissions for langfuse
+CREATE ROLE postgres WITH LOGIN PASSWORD 'postgres';
+GRANT ALL PRIVILEGES ON DATABASE postgres TO postgres;
 -- Create the database
 CREATE DATABASE aisheAI;
